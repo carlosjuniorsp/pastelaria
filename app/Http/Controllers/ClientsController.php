af05c7dae5041d89f293842bfd8eb407243b53e2
Carlos <carlosdossantosjuniorsp@gmail.com>
2023-10-18 10:11:36 -0300

    creating validations

--- app/Http/Controllers/ClientsController.php
+++ app/Http/Controllers/ClientsController.php
@@ -20,26 +20,29 @@ class ClientsController extends Controller
         $this->model = $clients;
     }
 
-    public function getAll()
+    public function index()
     {
         $clients = $this->model->all();
         return response()->json($clients);
     }
 
-    public function get($id)
+    public function show($id)
     {
-        $client = $this->model->find($id);
+        $client = $this->model->findOrFail($id);
         return response()->json($client);
     }
 
     public function store(Request $request)
     {
+
+        $this->validateForm($request);
         $client = $this->model->create($request->all());
         return response()->json($client);
     }
 
     public function update($id, Request $request)
     {
+        $this->validateForm($request);
         $client = $this->model->find($id)
             ->update($request->all());
         return response()->json($client);
@@ -50,5 +53,28 @@ class ClientsController extends Controller
         $this->model->find($id)
             ->delete();
         return response()->json(null);
+    }
+
+
+    private function validateForm($request)
+    {
+        $this->validate($request, [
+            'name' => 'required|min:3|max:100',
+            'email' => 'email|unique:clients|max:100|required',
+            'phone' => 'required|min:12|max:12',
+            'date_of_birth' => 'required',
+            'address' => 'required|min:3|max:100',
+            'complement' => 'required|min:3|max:50',
+            'district' => 'required|min:3|max:100',
+            'zip_code' => 'required|min:3|max:9',
+
+        ], [
+            'required' => "O :attribute é obrigatório",
+            'email' => "Informe um :attribute válido",
+            "min" => "O :attribute deve ter no mínimo :min caracteretes",
+            "max" => "O :attribute deve ter no máximo :max caracteretes",
+            "email.unique" => "Já existe um :attribute igual cadastrado, tente outro!"
+
+        ]);
     }
 }
